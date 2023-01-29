a8e142a9ef18e9924a53b4cdb5df84b63d568dd1
epsibaranis <epsibaranis@gmail.com>
2023-01-29 16:29:09 +0530

    Product of two real numbers

---
+++ Main.java
@@ -0,0 +1,14 @@
+package ProductOfTwoReal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ProductOfTwoReal a;
+		a=new ProductOfTwoReal();
+		a.set();
+		a.find();
+		a.display();
+
+	}
+
+}
---
+++ ProductOfTwoReal.java
@@ -0,0 +1,15 @@
+package ProductOfTwoReal;
+class ProductOfTwoReal {
+ private float x,y,p;
+ public void set() {
+	 x=10;
+	 y=20;
+ }
+ public void find() {
+	 p=x*y;
+ }
+ public void display() {
+	 System.out.println(p);
+ }
+
+}
